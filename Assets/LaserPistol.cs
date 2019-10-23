5dad2a57c4ab52cec7944d2690a6284d6866173e
Kevin Florio <kflorio98@gmail.com>
2019-10-22 21:37:00 -0700

    Initial commit

---
+++ Assets/LaserPistol.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPistol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Laser Pistol");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LaserGun.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGun : MonoBehaviour
+{
+
+
+    public Vector3 pos;
+    public Vector3 ang;
+    int dir = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        float x = pos.x;
+        float y = pos.y;
+        float z = pos.z;
+
+        ang = transform.localEulerAngles;
+        float angleX = ang.x;
+        float angleY = ang.y;
+        float angleZ = ang.z;
+
+        Debug.Log("Laser Gun translation coords ("+x+","+y+","+z+")");
+        Debug.Log("Laser Gun rotation coords (" + angleX + "," + angleY + "," + angleZ + ")");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(dir == 1)
+        {//Clockwise
+            transform.Rotate(0, 0, 0.8f);//Rotate gun clockwise
+            Camera.main.transform.Rotate(0, 0.8f, 0);//Rotate camera clockwise
+        }
+        else
+        {
+            transform.Rotate(0, 0, -0.8f);//Rotate camera counterclockwise
+            Camera.main.transform.Rotate(0, -0.8f, 0);
+        }
+
+        //Update the angles
+        ang = transform.localEulerAngles;
+        float angleX = ang.x;
+        float angleY = ang.y;
+        float angleZ = ang.z;
+
+        if(angleZ >= 180)
+        {
+            dir = -1;//Counter clockwise
+        }
+        if (angleZ <= 120)
+        {
+            dir = 1;//Counter clockwise
+        }
+
+
+        //Debug.Log("Laser Gun rotation coords (" + angleX + "," + angleY + "," + angleZ + ")");
+    }
+}
